4a38d1c965566721e8133bb205506f4545bbc07e
crr <crr@us.ibm.com>
2015-02-24 09:45:48 -0500

    Simplified rps minigame
    Change-Id: I4002011779d2d9575246ded3a5822efeeb22334b

--- test/rpsTest.py
+++ test/rpsTest.py
@@ -5,24 +5,24 @@ class RPSTest(unittest.TestCase):
 
     def test_shield_vs_lunge(self):
         winner = rps.judge(rps.SHIELD, rps.LUNGE)
-        self.assertEquals(-1, winner)
+        self.assertEquals(1, winner)
 
         winner = rps.judge(rps.LUNGE, rps.SHIELD)
-        self.assertEquals(1, winner)
+        self.assertEquals(2, winner)
 
     def test_lunge_vs_counter(self):
         winner = rps.judge(rps.LUNGE, rps.COUNTER)
-        self.assertEquals(-1, winner)
+        self.assertEquals(1, winner)
 
         winner = rps.judge(rps.COUNTER, rps.LUNGE)
-        self.assertEquals(1, winner)
+        self.assertEquals(2, winner)
 
     def test_counter_vs_shield(self):
         winner = rps.judge(rps.COUNTER, rps.SHIELD)
-        self.assertEquals(-1, winner)
+        self.assertEquals(1, winner)
 
         winner = rps.judge(rps.SHIELD, rps.COUNTER)
-        self.assertEquals(1, winner)
+        self.assertEquals(2, winner)
 
     def test_same_choice(self):
         winner = rps.judge(rps.SHIELD, rps.SHIELD)
--- util/rps.py
+++ util/rps.py
@@ -1,24 +1,10 @@
-SHIELD = 'Shield'
-COUNTER = 'Counter'
-LUNGE = 'Lunge'
+SHIELD = 0
+COUNTER = 1
+LUNGE = 2
 
-winning_list = {
-    'Shield': {
-        'Lunge': True
-    },
-    'Lunge': {
-        'Counter': True
-    },
-    'Counter': {
-        'Shield': True
-    }
-}
-
-# Return -1 if p1 wins, 0 for a tie, or 1 if p2 wins
+# Return 1 if p1 wins, 0 for a tie, or 2 if p2 wins
 def judge(p1_choice, p2_choice):
+    diff = p1_choice - p2_choice
+    return (diff + 3) if diff < 0 else diff
     if p1_choice == p2_choice:
         return 0
-    else:
-        # Return False if a value is not in the winning_list dictionary
-        p1_win = winning_list[p1_choice].get(p2_choice, False)
-        return -1 if p1_win else 1
