caf7095a00f63b5ec701ce6ccfa360cf0897fb19
crr <crr@us.ibm.com>
2015-02-25 15:43:42 -0500

    Add creation of source distribution
    Change-Id: I10a9ca07d51033498ab7fcc47679d09e90bb447b

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ from pybuilder.core import Author, init, use_plugin
 
 use_plugin("python.core")
 use_plugin("python.distutils")
+use_plugin("source_distribution")
 use_plugin("python.install_dependencies")
 
 # use_plugin("python.coverage")
@@ -9,7 +10,7 @@ use_plugin("python.unittest")
 use_plugin("python.flake8")
 use_plugin("python.frosted")
 
-default_task = ["clean", "publish"]
+default_task = ["clean", "publish", "build_source_distribution"]
 
 summary = "A game based on To Cry a Joust. Written in Python using PyGame."
 authors = [Author("Chris Rees", "serneum@gmail.com")]
