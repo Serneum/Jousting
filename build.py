6d8b12d2fadaed4d33bd3c2cf8ca1208fefd9638
crr <crr@us.ibm.com>
2015-02-25 17:01:17 -0500

    Add code coverage checks
    Change-Id: I68b64fc0bbc63f88f6be4413608413e46b60cf94

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ use_plugin("python.distutils")
 use_plugin("source_distribution")
 use_plugin("python.install_dependencies")
 
-# use_plugin("python.coverage")
+use_plugin("python.coverage")
 use_plugin("python.unittest")
 use_plugin("python.flake8")
 use_plugin("python.frosted")
@@ -19,6 +19,7 @@ license = "Apache License, Version 2.0"
 
 @init
 def initialize(project):
+    project.build_depends_on("coveralls")
     project.build_depends_on("mockito")
 
     project.set_property("verbose", True)
