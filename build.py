d309a1d7410b36dd2bc41101458e18ef7882dbb6
crr <crr@us.ibm.com>
2015-02-25 15:09:07 -0500

    Minor build script updates
    Change-Id: Ibb49ecb9629a9426cfc53f113cd20f6a217eed3a

--- build.py
+++ build.py
@@ -1,15 +1,20 @@
-from pybuilder.core import init, use_plugin
+from pybuilder.core import Author, init, use_plugin
 
 use_plugin("python.core")
-# use_plugin("python.distutils")
+use_plugin("python.distutils")
 use_plugin("python.install_dependencies")
 
-use_plugin("python.coverage")
+# use_plugin("python.coverage")
 use_plugin("python.unittest")
 use_plugin("python.flake8")
 use_plugin("python.frosted")
 
-default_task = ["install_dependencies", "publish"]
+default_task = ["clean", "publish"]
+
+summary = "A game based on To Cry a Joust. Written in Python using PyGame."
+authors = [Author("Chris Rees", "serneum@gmail.com")]
+url = "https://github.com/Serneum/Jousting"
+license = "Apache License, Version 2.0"
 
 @init
 def initialize(project):
