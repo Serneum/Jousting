62db941af9ddd975ecbfee7a222f40b50fc2ceb6
crr <crr@us.ibm.com>
2015-02-25 13:09:29 -0500

    Add Frosted dependency to build
    Change-Id: I8cb6b7eefc6906551a4369d5b00feccf92714481

--- build.py
+++ build.py
@@ -1,17 +1,18 @@
 from pybuilder.core import init, use_plugin
 
 use_plugin("python.core")
-use_plugin("python.unittest")
-use_plugin("python.coverage")
-use_plugin("python.flake8")
 # use_plugin("python.distutils")
 use_plugin("python.install_dependencies")
+
+use_plugin("python.coverage")
+use_plugin("python.unittest")
+use_plugin("python.flake8")
+use_plugin("python.frosted")
 
 default_task = ["install_dependencies", "publish"]
 
 @init
 def initialize(project):
-    # project.build.depends_on("coverage")
     project.build_depends_on("mockito")
 
     project.set_property("verbose", True)
@@ -19,3 +20,5 @@ def initialize(project):
     project.set_property("flake8_include_test_sources", True)
     project.set_property("flake8_break_build", True)
     project.set_property("flake8_include_test_sources", True)
+
+    project.set_property('frosted_include_test_sources', True)
