b106f0c9ab3a66b428b70ae8c7e6062d8e0e8ce8
Chris Rees <serneum@gmail.com>
2015-02-26 00:25:24 -0500

    Cleaned up tactics_rps tests

--- src/unittest/python/round/tactics_rps_tests.py
+++ src/unittest/python/round/tactics_rps_tests.py
@@ -8,86 +8,67 @@ from mockito import mock, when
 
 
 class TacticsCardRPSTest(unittest.TestCase):
+    def setUp(self):
+        self.controller = mock(Controller)
+        self.tactics_rps = TacticsCardRPS(self.controller)
+
+        self.p1 = Knight("Name")
+        self.p2 = Knight("Name")
+        when(self.controller).get_p1().thenReturn(self.p1)
+        when(self.controller).get_p2().thenReturn(self.p2)
+
     def test_rps_tie(self):
-        controller = mock(Controller)
-        tactics_rps = TacticsCardRPS(controller)
+        when(self.p1).get_tactical_card().thenReturn(rps.SHIELD)
+        when(self.p2).get_tactical_card().thenReturn(rps.SHIELD)
 
-        p1 = Knight("Name")
-        p2 = Knight("Name")
-        when(controller).get_p1().thenReturn(p1)
-        when(controller).get_p2().thenReturn(p2)
-
-        when(p1).get_tactical_card().thenReturn(rps.SHIELD)
-        when(p2).get_tactical_card().thenReturn(rps.SHIELD)
-
-        tactics_rps.do_tactics_rps()
-        self.assertTrue(p1.get_accept_heavy_blows())
-        self.assertTrue(p2.get_accept_heavy_blows())
+        self.tactics_rps.do_tactics_rps()
+        self.assertTrue(self.p1.get_accept_heavy_blows())
+        self.assertTrue(self.p2.get_accept_heavy_blows())
 
     def test_rps_p1_wins_with_shield(self):
-        controller = mock(Controller)
-        tactics_rps = TacticsCardRPS(controller)
+        when(self.controller).get_p1().thenReturn(self.p1)
+        when(self.controller).get_p2().thenReturn(self.p2)
 
-        p1 = Knight("Name")
-        p2 = Knight("Name")
-        when(controller).get_p1().thenReturn(p1)
-        when(controller).get_p2().thenReturn(p2)
+        when(self.p1).get_tactical_card().thenReturn(rps.SHIELD)
+        when(self.p2).get_tactical_card().thenReturn(rps.LUNGE)
 
-        when(p1).get_tactical_card().thenReturn(rps.SHIELD)
-        when(p2).get_tactical_card().thenReturn(rps.LUNGE)
-
-        tactics_rps.do_tactics_rps()
-        self.assertFalse(p1.get_accept_heavy_blows())
-        self.assertTrue(p2.get_accept_heavy_blows())
+        self.tactics_rps.do_tactics_rps()
+        self.assertFalse(self.p1.get_accept_heavy_blows())
+        self.assertTrue(self.p2.get_accept_heavy_blows())
 
     def test_rps_p2_wins_with_shield(self):
-        controller = mock(Controller)
-        tactics_rps = TacticsCardRPS(controller)
+        when(self.controller).get_p1().thenReturn(self.p1)
+        when(self.controller).get_p2().thenReturn(self.p2)
 
-        p1 = Knight("Name")
-        p2 = Knight("Name")
-        when(controller).get_p1().thenReturn(p1)
-        when(controller).get_p2().thenReturn(p2)
+        when(self.p1).get_tactical_card().thenReturn(rps.LUNGE)
+        when(self.p2).get_tactical_card().thenReturn(rps.SHIELD)
 
-        when(p1).get_tactical_card().thenReturn(rps.LUNGE)
-        when(p2).get_tactical_card().thenReturn(rps.SHIELD)
-
-        tactics_rps.do_tactics_rps()
-        self.assertTrue(p1.get_accept_heavy_blows())
-        self.assertFalse(p2.get_accept_heavy_blows())
+        self.tactics_rps.do_tactics_rps()
+        self.assertTrue(self.p1.get_accept_heavy_blows())
+        self.assertFalse(self.p2.get_accept_heavy_blows())
 
     def test_rps_p1_wins_not_shield(self):
-        controller = mock(Controller)
-        tactics_rps = TacticsCardRPS(controller)
+        when(self.controller).get_p1().thenReturn(self.p1)
+        when(self.controller).get_p2().thenReturn(self.p2)
 
-        p1 = Knight("Name")
-        p2 = Knight("Name")
-        when(controller).get_p1().thenReturn(p1)
-        when(controller).get_p2().thenReturn(p2)
+        when(self.p1).get_tactical_card().thenReturn(rps.COUNTER)
+        when(self.p2).get_tactical_card().thenReturn(rps.SHIELD)
 
-        when(p1).get_tactical_card().thenReturn(rps.COUNTER)
-        when(p2).get_tactical_card().thenReturn(rps.SHIELD)
-
-        tactics_rps.do_tactics_rps()
-        self.assertTrue(p1.get_accept_heavy_blows())
-        self.assertTrue(p2.get_accept_heavy_blows())
-        self.assertTrue(p1.get_won_rps())
-        self.assertFalse(p2.get_won_rps())
+        self.tactics_rps.do_tactics_rps()
+        self.assertTrue(self.p1.get_accept_heavy_blows())
+        self.assertTrue(self.p2.get_accept_heavy_blows())
+        self.assertTrue(self.p1.get_won_rps())
+        self.assertFalse(self.p2.get_won_rps())
 
     def test_rps_p1_wins_not_shield(self):
-        controller = mock(Controller)
-        tactics_rps = TacticsCardRPS(controller)
+        when(self.controller).get_p1().thenReturn(self.p1)
+        when(self.controller).get_p2().thenReturn(self.p2)
 
-        p1 = Knight("Name")
-        p2 = Knight("Name")
-        when(controller).get_p1().thenReturn(p1)
-        when(controller).get_p2().thenReturn(p2)
+        when(self.p1).get_tactical_card().thenReturn(rps.COUNTER)
+        when(self.p2).get_tactical_card().thenReturn(rps.LUNGE)
 
-        when(p1).get_tactical_card().thenReturn(rps.COUNTER)
-        when(p2).get_tactical_card().thenReturn(rps.LUNGE)
-
-        tactics_rps.do_tactics_rps()
-        self.assertTrue(p1.get_accept_heavy_blows())
-        self.assertTrue(p2.get_accept_heavy_blows())
-        self.assertFalse(p1.get_won_rps())
-        self.assertTrue(p2.get_won_rps())
+        self.tactics_rps.do_tactics_rps()
+        self.assertTrue(self.p1.get_accept_heavy_blows())
+        self.assertTrue(self.p2.get_accept_heavy_blows())
+        self.assertFalse(self.p1.get_won_rps())
+        self.assertTrue(self.p2.get_won_rps())
